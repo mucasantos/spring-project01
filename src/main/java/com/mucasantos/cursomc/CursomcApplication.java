a518ab146877f555a16c7fd7cb18b4af6bbf6811
Samuel Santos <mucasantos@gmail.com>
2022-07-07 14:07:19 -0300

    Primeira classe criada com GET

--- src/main/java/com/mucasantos/cursomc/CursomcApplication.java
+++ src/main/java/com/mucasantos/cursomc/CursomcApplication.java
@@ -1,4 +1,4 @@
-package com.mucasantos.cursomc.cursomc;
+package com.mucasantos.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
